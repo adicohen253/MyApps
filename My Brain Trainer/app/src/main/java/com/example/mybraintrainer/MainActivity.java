c4a329dfd97200b7ce5054ab9a20b4b83135cfb4
adicohen253 <55681305+adicohen253@users.noreply.github.com>
2020-12-02 15:26:14 +0200

    My Brain Trainer - update

--- My Brain Trainer/app/src/main/java/com/example/mybraintrainer/MainActivity.java
+++ My Brain Trainer/app/src/main/java/com/example/mybraintrainer/MainActivity.java
@@ -14,11 +14,15 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
+@SuppressLint("SetTextI18n")
 public class MainActivity extends AppCompatActivity {
     TextView scoreTextView;
     TextView timeLeftTextView;
     TextView expressionTextView;
+    TextView answerTextView;
+    Button restartButton;
     GridLayout optionsGridLayout;
+
 
     Button [] optionButtons;
     String [] operators = {" + ", " - ", " * ", " / ", " % "};
@@ -27,20 +31,14 @@ public class MainActivity extends AppCompatActivity {
     Random random = new Random();
     private int numberOfQuestions = 0;
     private int correctAnswers = 0;
-    private final int SecondsToPlay = 10; //time in seconds for the game
+    private final int SecondsToPlay = 90; //time in seconds for the game
 
-
-    public void startGame(View startButton)
+    public void startGame()
     {
-        startButton.setVisibility(View.INVISIBLE);
-        scoreTextView.setVisibility(View.VISIBLE);
-        timeLeftTextView.setVisibility(View.VISIBLE);
-        expressionTextView.setVisibility(View.VISIBLE);
-        optionsGridLayout.setVisibility(View.VISIBLE);
+
         new CountDownTimer(SecondsToPlay * 1000, 1000)
         {
 
-            @SuppressLint("SetTextI18n")
             @Override
             public void onTick(long l) {
                 timeLeftTextView.setText(((int)l/1000 + 1) + "s");
@@ -48,16 +46,16 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-                timeLeftTextView.setText("0");
+                timeLeftTextView.setText("0s");
                 for (Button optionButton : optionButtons) {
                     optionButton.setEnabled(false);
+                    restartButton.setVisibility(View.VISIBLE);
                 }
             }
         }.start();
         buildExercise();
     }
 
-    @SuppressLint("SetTextI18n")
     private void buildExercise()
     {
         String operator = operators[random.nextInt(5)];
@@ -93,13 +91,41 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @SuppressLint("SetTextI18n")
+    public void startPlay(View view)
+    {
+        view.setVisibility(View.INVISIBLE);
+        scoreTextView.setVisibility(View.VISIBLE);
+        timeLeftTextView.setVisibility(View.VISIBLE);
+        expressionTextView.setVisibility(View.VISIBLE);
+        optionsGridLayout.setVisibility(View.VISIBLE);
+        startGame();
+    }
+
+    public void restartGame(View view)
+    {
+        restartButton.setVisibility(View.INVISIBLE);
+        optionsGridLayout.setEnabled(true);
+        scoreTextView.setText(R.string.starting_score);
+        answerTextView.setText("");
+        answerTextView.setVisibility(View.INVISIBLE);
+        numberOfQuestions = 0;
+        correctAnswers = 0;
+        startGame();
+    }
+
     public void chooseAnswer(View view)
     {
+        if(answerTextView.getVisibility() != View.VISIBLE)
+            answerTextView.setVisibility(View.VISIBLE);
         numberOfQuestions++;
         if((int) view.getTag() == 1)
         {
             correctAnswers++;
+            answerTextView.setText(R.string.correct);
+        }
+        else
+        {
+            answerTextView.setText(R.string.wrong);
         }
         scoreTextView.setText(correctAnswers + "/" + numberOfQuestions);
         buildExercise();
@@ -110,6 +136,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        restartButton = findViewById(R.id.restartGameButton);
+        answerTextView = findViewById(R.id.lastAnswerTextView);
         scoreTextView = findViewById(R.id.scoreTextView);
         timeLeftTextView = findViewById(R.id.timeLeftTextView);
         expressionTextView = findViewById(R.id.expressionTextView);
