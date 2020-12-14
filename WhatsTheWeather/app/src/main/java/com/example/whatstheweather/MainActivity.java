5c5365dc2aef9bb46e1c107b808b81236975c2f3
adicohen253 <55681305+adicohen253@users.noreply.github.com>
2020-12-14 14:27:47 +0200

    Whats the weather - update

--- WhatsTheWeather/app/src/main/java/com/example/whatstheweather/MainActivity.java
+++ WhatsTheWeather/app/src/main/java/com/example/whatstheweather/MainActivity.java
@@ -1,28 +1,44 @@
 package com.example.whatstheweather;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.JsonReader;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-
 public class MainActivity extends AppCompatActivity {
-    private class weatherDownloader extends AsyncTask<String, Void, JSONObject>
+    final String MyUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=f09e9ea7ed975cf747f7d5e1bd1cab8d&q=";
+    Button submitButton;
+    EditText cityEditText;
+    TextView mainWeatherTextView;
+    TextView descriptionWeatherTextView;
+    TextView cityTextView;
+
+    private static class weatherDownloader extends AsyncTask<String, Void, JSONObject>
     {
         @Override
         protected JSONObject doInBackground(String... urls) {
+            StringBuilder data = new StringBuilder();
             HttpURLConnection urlConnection;
-            try {
-                urlConnection = (HttpURLConnection) (new URL(urls[0])).openConnection();
-                InputStreamReader reader = new InputStreamReader(urlConnection.getInputStream());
+            try
+            {
+                urlConnection = (HttpURLConnection) new URL(urls[0]).openConnection();
+                InputStreamReader streamReader = new InputStreamReader(urlConnection.getInputStream());
+                int temp = streamReader.read();
+                while (temp != -1)
+                {
+                    data.append((char) temp);
+                    temp = streamReader.read();
+                }
+                return new JSONObject(data.toString());
             }
             catch (Exception e)
             {
@@ -33,19 +49,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void displayWeather(View view)
+    {
+        cityEditText.setEnabled(false);
+        cityEditText.setEnabled(true);
+
+        weatherDownloader downloader = new weatherDownloader();
+        String city = cityEditText.getText().toString();
+
+        cityEditText.setText("");
+        cityTextView.setText("");
+        mainWeatherTextView.setText("");
+        descriptionWeatherTextView.setText("");
+
+        JSONObject weatherData;
+        try {  // trying to get data
+            weatherData = downloader.execute(MyUrl + city).get();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Cant get weather data!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        try {  // checking if there is weather information
+            weatherData = weatherData.getJSONArray("weather").getJSONObject(0);
+        }
+        catch (Exception e) { // input was invalid
+            Toast.makeText(this, "City not found!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        try {
+            mainWeatherTextView.setText(weatherData.getString("main"));
+            descriptionWeatherTextView.setText(weatherData.getString("description"));
+        } catch (JSONException e) {
+            return;
+        }
+        cityTextView.setText(city);
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        JSONObject weatherData = null;
-        try {
-            weatherData = new weatherDownloader().execute("http://api.openweathermap.org/data/2.5/weather?q=J9e9ea7ed975cf747f7d5e1bd1cab8d").get();
-            int y = 3;
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
+
+        submitButton = findViewById(R.id.submitButton);
+        cityEditText = findViewById(R.id.cityInputEditText);
+        mainWeatherTextView = findViewById(R.id.mainWeatherTextView);
+        descriptionWeatherTextView = findViewById(R.id.descriptionWeatherTextView);
+        cityTextView = findViewById(R.id.cityTextView);
+
 
     }
 }
